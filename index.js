183f6b2f7aded1df7ac12f7654ff56dc219010b0
firejoust <42525540+firejoust@users.noreply.github.com>
2023-04-09 11:36:51 +1000

    use setter function as argument

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const Path = require("./src/path.js")
 
+const Set = (instance, callback) => {
+    return (...args) => {
+        callback(...args)
+        return instance
+    }
+}
+
 module.exports.plugin = function inject(bot) {
     bot.pathfinder = new Plugin(bot)
 }
 
 function Plugin(bot) {
-    this.Path = Path.inject(bot)
+    this.Path = Path.inject(bot, Set)
 }
