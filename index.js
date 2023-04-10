da11fd8ce14cc447922db586a1282acf6f0d9086
firejoust <42525540+firejoust@users.noreply.github.com>
2023-04-10 13:44:40 +1000

    Remove new constructor for regular function

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 const Path = require("./src/path.js")
 const Goals = require("./src/goals.js")
 
-const Setter = (instance, callback) => {
-    return (...args) => {
-        callback(...args)
-        return instance
-    }
-}
+module.exports.goals = Goals()
 
 module.exports.plugin = function inject(bot) {
     bot.pathfinder = new Plugin(bot)
 }
 
-module.exports.goals = (
-    Goals.inject(Setter)
-)
-
 function Plugin(bot) {
-    this.Path = Path.inject(bot, Setter)
+    this.Path = Path.inject(bot)
 }
