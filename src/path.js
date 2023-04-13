24610ef62c61f0bd8634311beb64b8e1539c256a
firejoust <42525540+firejoust@users.noreply.github.com>
2023-04-13 15:21:46 +1000

    WIP: ascend/descend single blocks - Descending blocks has not been implemented yet - Currently checking 4 vertical blocks, need to check 5 (for climbing)

--- src/path.js
+++ src/path.js
@@ -60,7 +60,66 @@ function hazardWeight(hazards, position) {
     return multiplier
 }
 
+function getMaxY(shapes) {
+    let maxY = 0
+    for (let box of shapes) {
+        if (box[1] > maxY) {
+            maxY = box[1]
+        }
+        if (box[4] > maxY) {
+            maxY = box[4]
+        }
+    }
+    return maxY
+}
+
+function getMinY(shapes) {
+    let minY = Infinity
+    for (let box of shapes) {
+        if (box[1] < minY) {
+            minY = box[1]
+        }
+        if (box[4] < minY) {
+            minY = box[4]
+        }
+    }
+    return minY
+}
+
+function HeightMap(data) {
+    for (let block of data.blocksArray) {
+        const stateMax = block.maxStateId - block.minStateId
+
+        if (stateMax === 0) {
+            this[block.minStateId || block.stateId] = {
+                max: getMaxY(block.shapes),
+                min: getMinY(block.shapes)
+            }
+        } else
+
+        if (block.stateShapes === undefined) {
+            for (let i = 0; i <= stateMax; i++) {
+                this[block.minStateId + i] = {
+                    max: getMaxY(block.shapes),
+                    min: getMinY(block.shapes)
+                }
+            }
+        } else
+
+        {
+            for (let i = 0; i <= stateMax; i++) {
+                this[block.minStateId + i] = {
+                    max: getMaxY(block.stateShapes[i]),
+                    min: getMinY(block.stateShapes[i])
+                }
+            }
+        }
+    }
+}
+
 module.exports.inject = function inject(bot) {
+    const heightMap = new HeightMap(bot.registry)
+
     return function Path(goal, ...hazards) {
         let { avoid, depth, blocks, timeout } = Defaults
         this.avoid = Setter(this, _ => avoid = _)
@@ -185,15 +244,163 @@ module.exports.inject = function inject(bot) {
                 return Boolean(avoid[block.name])
             }
         }
+
+        function canMoveFrom(currentPos, nextPos) {
+            let floor1, floor2, ceiling1, ceiling2
+            floor1 = floor2 = 0
+            ceiling1 = ceiling2 = 0
+            
+            /*
+                Get the amount of space in the next node
+            */
+            {
+                const yf_2 = bot.blockAt(nextPos.offset(0, -1, 0))
+                const y0_2 = bot.blockAt(nextPos)
+                const y1_2 = bot.blockAt(nextPos.offset(0, 1, 0))
+                const y2_2 = bot.blockAt(nextPos.offset(0, 2, 0))
+
+                const yf_2_empty = emptyBlock(yf_2) 
+                const y0_2_empty = emptyBlock(y0_2)
+                const y1_2_empty = emptyBlock(y1_2)
+                const y2_2_empty = emptyBlock(y2_2)
+
+                /*
+                    NOTE: this logic will be changed in the future
+                    (allow descending nods)
+                */
+                if (yf_2_empty && y0_2_empty) {
+                    console.log("false\n")
+                    return false
+                }
+
+                floor2 = y0_2_empty
+                ? 2 - heightMap[yf_2.stateId].max
+                : yf_2_empty
+                ? 1 - heightMap[y0_2.stateId].max
+                : 2 - Math.max(1 + heightMap[y0_2.stateId].max, heightMap[yf_2.stateId].max)
+
+                ceiling2 = y1_2_empty && y2_2_empty
+                ? 2
+                : y1_2_empty
+                ? 1 + heightMap[y2_2.stateId].min
+                : heightMap[y1_2.stateId].min
+
+                /*
+                // see if we can climb the ceiling block
+                if (ceiling2 < 1 && y2_2_empty) {
+                    const y3_2 = bot.blockAt(nextPos.offset(0, 3, 0))
+                    const y3_2_empty = emptyBlock(y3_2)
+
+                    if (y3_2_empty) {
+                        const feet = 1 - heightMap[y1_2.stateId].max
+                        ceiling2 = feet + 2
+                    } else {
+                        const head = heightMap[y3_2.stateId].min
+                        const feet = 1 - heightMap[y1_2.stateId].max
+                        ceiling2 = feet + 1 + head
+                    }
+                }
+                */
+            }
+
+            console.log(`nextPos: ${floor2} + ${ceiling2} < ${bot.physics.playerHeight} = ${floor2 + ceiling2 < bot.physics.playerHeight}`)
+
+            // not enough space to get to next node
+            if (floor2 + ceiling2 < bot.physics.playerHeight) {
+                console.log("false\n")
+                return false
+            }
+
+            /*
+                Get the amount of space in the current node
+            */
+            {
+                const yf_1 = bot.blockAt(currentPos.offset(0, -1, 0))
+                const y0_1 = bot.blockAt(currentPos)
+                const y1_1 = bot.blockAt(currentPos.offset(0, 1, 0))
+                const y2_1 = bot.blockAt(currentPos.offset(0, 2, 0))
+
+                const yf_1_empty = emptyBlock(yf_1)
+                const y0_1_empty = emptyBlock(y0_1)
+                const y1_1_empty = emptyBlock(y1_1)
+
+                floor1 = yf_1_empty && y0_1_empty
+                ? 2
+                : y0_1_empty // current node is empty
+                ? 2 - heightMap[yf_1.stateId].max
+                : yf_1_empty // current node isn't empty
+                ? 1 - heightMap[y0_1.stateId].max
+                : 2 - Math.max(1 + heightMap[y0_1.stateId].max, heightMap[yf_1.stateId].max) // both nodes are solid, pick the tallest block
+
+                ceiling1 = y1_1_empty && emptyBlock(y2_1)
+                ? 2
+                : y1_1_empty
+                ? 1 + heightMap[y2_1.stateId].min
+                : heightMap[y1_1.stateId].min // either y2 is empty, or none are empty
+            }
+
+            console.log(`currentPos: ${floor1} + ${ceiling1} < ${bot.physics.playerHeight} = ${floor1 + ceiling1 < bot.physics.playerHeight}`)
+
+
+            const floor = Math.min(floor1, floor2)
+            const ceiling = Math.min(ceiling1, ceiling2)
+
+            console.log(`newPos: ${floor} + ${ceiling} < ${bot.physics.playerHeight} = ${floor + ceiling < bot.physics.playerHeight}`)
+
+
+            // cannot leave the current node
+            if (floor + ceiling < bot.physics.playerHeight) {
+                console.log("false\n")
+                return false
+            }
+
+            console.log(`floor2 - floor1: ${Math.abs(floor2 - floor1)}`)
+
+            // change coditions for when ceiling2 was changed (block climb)
+
+
+            nextPos.y -= floor2 - floor1
+
+            if (unsafeBlock(bot.blockAt(nextPos))) {
+                console.log("false\n")
+                return false
+            }
+
+            console.log("true\n")
+            return true
+        }
     
+        /*
         function canMoveFrom(currentPos, nextPos) {
             const y1 = bot.blockAt(nextPos.offset(0, 1, 0))
-            // cannot proceed if block obstruction at head height
+
+            // block obstruction at head height, check if we can go under it
             if (solidBlock(y1)) {
-                return false
+                if (unsafeBlock(y1)) {
+                    return false
+                }
+
+                const yf = bot.blockAt(currentPos.offset(0, -1, 0))
+
+                if (unsafeBlock(yf) || emptyBlock(yf)) {
+                    return false
+                }
+
+                const y0 = bot.blockAt(nextPos)
+                const feet = Math.abs(heightMap[yf.stateId].max - 1)
+                const head = emptyBlock(y0)
+                ? 1 + heightMap[y1.stateId].min
+                : heightMap[y0.stateId].min
+
+                console.log(`${feet} + ${head} >= ${bot.physics.playerHeight}`)
+
+                if (feet + head >= bot.physics.playerHeight) {
+                    return true
+                }
+
             } else
         
-            // no block obstruction at head height
+            // no block obstruction at head height, check the next block down
             if (emptyBlock(y1)) {
                 const y0 = bot.blockAt(nextPos)
         
@@ -205,7 +412,7 @@ module.exports.inject = function inject(bot) {
                     return false
                 }
         
-                // check if the player can descend
+                // keep going down until we hit a solid block
                 if (emptyBlock(y0)) {
                     const lastPos = nextPos.clone()
                     const descendPos = nextPos.offset(0, -1, 0)
@@ -222,27 +429,36 @@ module.exports.inject = function inject(bot) {
                             return true
                         }
         
-                        lastPos.translate(0, -1, 0)
-                        descendPos.translate(0, -1, 0)
+                        lastPos.y--
+                        descendPos.y--
                     }
                 } else
         
-                // check if the player can climb
+                // check if we can climb up the block
                 if (solidBlock(y0)) {
                     const y2_0 = bot.blockAt(currentPos.offset(0, 2, 0))
-                    const y2_1 = bot.blockAt(nextPos.offset(0, 2, 0))
         
                     if (unsafeBlock(y2_0)) {
                         return false
                     }
-        
+
+                    const y2_1 = bot.blockAt(nextPos.offset(0, 2, 0))
+
                     if (unsafeBlock(y2_1)) {
                         return false
                     }
-        
+
+                    const yf = bot.blockAt(currentPos.offset(0, -1, 0))
+
+                    if (unsafeBlock(yf) || emptyBlock(yf)) {
+                        return false
+                    }
+
                     if (emptyBlock(y2_0) && emptyBlock(y2_1)) {
-                        nextPos.translate(0, 1, 0)
-                        return true
+                        if (heightMap[y0.stateId].max - heightMap[yf.stateId].max <= 0.25) {
+                            nextPos.y += 1
+                            return true
+                        }
                     }
                 }
             }
@@ -250,5 +466,6 @@ module.exports.inject = function inject(bot) {
             // cannot get to the next node
             return false
         }
+        */
     }
 }
